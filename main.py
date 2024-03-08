a944e0a2fb9918c2590912f5400dda17d3306a95
angelsusum <angelsusum@gmail.com>
2024-03-08 15:00:14 +0900

    김수민 2021110134

--- main.py
+++ main.py
@@ -2,4 +2,5 @@
 
 name = input("이름을 입력하세요: ")
 studentNum = input("학번을 입력하세요: ")
-print("이름: ", name, "\n학번: ", studentNum)
+major = input("학과를 입력하세요: ")
+print("이름: ", name, "\n학번: ", studentNum, "\n학과: ", major)
